e7f842a984172d179b1c96d22d7385a01182cd9d
ValentinaCollareta <valencollareta2@gmail.com>
2021-10-08 19:08:36 -0300

    add 04 if else exercises

---
+++ exercises/04_if_else.js
@@ -0,0 +1,18 @@
+/******************************
+* 4. If Else
+* ****************************/
+
+/*  a) Create a random number between 0 and 1 using the function Math.random(), if the value is
+greater or equal than 0,5 show an alert with the message “Greater than 0,5” and if not 
+an alert with the message “Lower than 0,5”. */
+
+
+/*  b) Create a variable "Age" with a integer number between 0 and 100 and show the
+next alert messages:
+i) “Baby” if the age is less than 2 years old;
+ii) “Kid” if the age is between 2 and 12 years old;
+iii) “Teenager” between 13 and 19 years old;
+iv) “Young adult” between 20 and 30 years old;
+v) “Adult” between 31 and 60 years old;
+vi) “Old adult” between 61 and 75 years old;
+vii) “Elderly” if is greater than 75 years old. */
