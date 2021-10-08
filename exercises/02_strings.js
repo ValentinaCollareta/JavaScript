1763c5d391f685ada62d875a0912592a84cc2382
ValentinaCollareta <valencollareta2@gmail.com>
2021-10-08 17:23:55 -0300

    add 02 strings exercises

---
+++ exercises/02_strings.js
@@ -0,0 +1,32 @@
+/******************************
+* 2. Strings
+* ****************************/
+
+/*  a. Create a string variable with at least 10 characters and convert all the text to
+upper case letters (using toUpperCase).   */
+
+
+/*  b. Create a string variable with at least 10 characters and make a new string
+with the first 5 characters storing the result in a new variable (using
+substring).   */
+
+
+/*  c. Create a string variable with at least 10 characters and make a new string
+with the last 3 characters storing the result in a new variable (using
+substring).   */
+
+
+/*  d) Create a string variable with at least 10 characters and make a new string
+upper case the first letter and let the others in lower case. Store the result in
+a new variable (using substring, toUpperCase, toLowerCase and +).   */
+
+
+/*  e) Create a string variable with at least 10 characters and a space.
+Found the position of the first space and store the result in a new variable (using
+indexOf).   */
+
+
+/*  f) Create a string variable with at least 2 long words (10 characters and
+some spaces between words). Using the methods of the previous exercises to make a
+new string, upper case the first letter of all words and let the others in lower case.
+(using indexOf, substring, toUpperCase, toLowerCase and +).   */
