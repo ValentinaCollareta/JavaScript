1d9a65c7639dd00d3a5e61b9f8ceb3f77b4ec6bc
ValentinaCollareta <valencollareta2@gmail.com>
2021-10-08 16:54:45 -0300

    first commit

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,14 @@
+/******************************
+* 1. Variables and Operators
+* ****************************/
+
+/*  a. Create two numeric variables and use the sum operator to store the value of the sum
+of both numbers in another variable.  */
+
+
+/*  b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er
+variable.   */
+
+
+/*  c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
+del string) guardando el resultado de la suma en una 3er variable (utilizar length).    */
