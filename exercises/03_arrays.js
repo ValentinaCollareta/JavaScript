a289eaf602a412b9fd819e87f6705345b0410bf0
ValentinaCollareta <valencollareta2@gmail.com>
2021-10-08 18:35:26 -0300

    add 03 arrays exercises

---
+++ exercises/03_arrays.js
@@ -0,0 +1,26 @@
+/******************************
+* 2. Arrays
+* ****************************/
+
+/*  a. Using this array: ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"] show it in the
+console the 5 and 11 month (using console.log).    */
+
+
+/*  b. Order the array before alphabetically and show it in the console (using sort).    */
+
+
+/*  c. Add an element at the start and the end of the array (using unshift y push).    */
+
+
+/*  d. Delete and element of the start and the end of the array (using shift y pop).    */
+
+
+/*  e. Reverse the order of the array (using reverse).    */
+
+
+/*  f. Put together all the elements of the array in one single string where every month be separated
+by a dash - (using join).    */
+
+
+/*  g. Create a copy of the array with the months from May to November (using slice).    */
