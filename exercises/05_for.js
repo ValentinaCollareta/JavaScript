4a98a6d1f54387d5f2a99d23c17aa06b50560c1a
ValentinaCollareta <valencollareta2@gmail.com>
2021-10-09 05:59:05 -0300

    add 05 for exercises

---
+++ exercises/05_for.js
@@ -0,0 +1,21 @@
+/******************************
+* 5. For
+* ****************************/
+
+/* a. Create an array which contains 5 words and iterate over an array using
+JavaScript For to show an alert with each word.*/
+
+/* b. Convert the first letter of every word of the last array to upper case
+and show an alert for every modified word*/
+
+/* c. Create a variable called "sentence" that cointain an empty string
+then iterate over the array of the first point with for loop to store 
+every word inside the variable sentence. At the end, show an unique alert
+with the complete string.*/
+
+/* d. Create an empty array and iterate over using for loop 10 times.
+Fill the array with the number of the iteration, it means that at the end
+of the execution of the for loop should have 10 elements inside the array,
+from 0 to 9. Show in the console of the navigator the final array
+(using console.log)*/
+
